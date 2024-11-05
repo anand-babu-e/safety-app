d08ac9029b0648fc9bc31c06ddbef51c4be16c4c
Anand Babu Elanjikal(UST <287918@ust.com>
2024-11-06 00:00:58 +0530

    modified serializers.py

--- backend/project/app1/serializers.py
+++ backend/project/app1/serializers.py
@@ -10,17 +10,21 @@ class SignupSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'confirm_password']
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'confirm_password': {'write_only': True},
+            'email': {'required': True}  
+        }
+    
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already registered.")
+        return value
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Passwords do not match.")
         return data
-
-    def validate_email(self, value):
-        if not value:
-            raise serializers.ValidationError("Email is required.")
-        return value
 
     def create(self, validated_data):
         validated_data.pop('confirm_password')
@@ -31,13 +35,17 @@ class EmergencyContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = EmergencyContact
         fields = '__all__'
-        read_only_fields = ['user'] 
+        extra_kwargs = {
+            'user': {'read_only': True}  
+        } 
 
 class SOSRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = SOSRequest
         fields = '__all__'
-        read_only_fields = ['user'] 
+        extra_kwargs = {
+            'user': {'read_only': True}  
+        }
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
