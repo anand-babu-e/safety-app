eb682291761168c7a704cd567ad2c71ea27bc9ae
Aleena Maria Paul(UST <288037@ust.com>
2024-11-09 10:18:08 +0530

    created admin

--- backend/project/app1/admin.py
+++ backend/project/app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import EmergencyContact, SOSRequest, Incident 
+
+admin.site.register([EmergencyContact, SOSRequest, Incident])
 
 # Register your models here.
