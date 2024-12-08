024aed4226572bded3bb484a05f0d7f19fbd8a2d
Anand Babu Elanjikal(UST <287918@ust.com>
2024-12-08 12:12:15 +0530

    updated the serializers.py

--- backend/project/app1/serializers.py
+++ backend/project/app1/serializers.py
@@ -1,5 +1,6 @@
+from unittest.util import _MAX_LENGTH
 from rest_framework import serializers
-from .models import EmergencyContact, Incident, SOSRequest
+from .models import EmergencyContact, SOSRequest, Incident
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
@@ -12,24 +13,32 @@ class SignupSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'confirm_password']
         extra_kwargs = {
             'password': {'write_only': True},
-            'confirm_password': {'write_only': True},
             'email': {'required': True}  
         }
-    
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("This email is already registered.")
-        return value
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
             raise serializers.ValidationError("Passwords do not match.")
+        if User.objects.filter(email=data['email']).exists():
+            raise serializers.ValidationError("Email is already registered.")
         return data
 
     def create(self, validated_data):
-        validated_data.pop('confirm_password')
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            password = validated_data['password'],
+        )
+        user.is_active = False
+        user.save()
         return user
+
+class OTPRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+class OTPVerificationSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    otp = serializers.CharField(max_length = 6)
 
 class EmergencyContactSerializer(serializers.ModelSerializer):
     class Meta:
